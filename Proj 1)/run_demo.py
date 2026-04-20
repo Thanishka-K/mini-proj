fdb782c41dc97e1bb71bb062f952e13f4de7356a
Snorlaxx <thanishka.k06@gmail.com>
2026-04-20 19:31:10 +0530

    Add run_demo.py for automated EDA demonstration
    This script runs the engine on a sample dataset for automated EDA.

---
+++ Proj 1)/run_demo.py
@@ -0,0 +1,7 @@
+from eda_engine import run_eda
+
+# Here This script runs the engine on a sample dataset, im using this as an optional file
+if __name__ == "__main__":
+    print("🚀 Starting Automated EDA Demo...")
+    run_eda("sample_data.csv")
+    print("✅ Demo Complete! Check the /eda_reports folder.")
